1865bba4a806b837b22d109a3990601737014c9c
gktkumartripathi <gtgaurav1005@gmail.com>
2022-07-14 15:55:07 +0530

    sd

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const userController = require('../controllers/userController')
-const bookController = require('../controllers/bookController')
-
 const middleware = require('../middleware/auth')
 const router = express.Router();
 
@@ -11,12 +9,6 @@ const router = express.Router();
 router.post('/register',userController.createUser)
 router.post("/login", userController.login);
 router.put("/updatePassword/:userId",middleware.auth, userController.update)
-
-//   ==============Book Api================================
-
-router.post("/books", middleware.auth, bookController.createBook)
-router.get("/books",  bookController.getBooks )
-router.get("/books/:bookId", bookController.getBooksById)
 
 
 module.exports = router;
